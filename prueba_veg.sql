5f45e9989c344f6c6edf3ff3f05fe7541ad5d6b7
Emmanuel Damian Peña <emmanuel.damian@infotec.mx>
2020-12-03 19:07:38 -0600

    base de datos

---
+++ prueba_veg.sql
@@ -0,0 +1,126 @@
+/*
+SQLyog Ultimate v11.11 (64 bit)
+MySQL - 5.5.5-10.1.32-MariaDB : Database - pruebaveg
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`pruebaveg` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `pruebaveg`;
+
+/*Table structure for table `alumnos` */
+
+DROP TABLE IF EXISTS `alumnos`;
+
+CREATE TABLE `alumnos` (
+  `id_alumno` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `a_paterno` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `a_materno` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `id_carrera` int(11) DEFAULT NULL,
+  `id_grupo` int(11) DEFAULT NULL,
+  `activo` int(11) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id_alumno`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `alumnos` */
+
+/*Table structure for table `carreras` */
+
+DROP TABLE IF EXISTS `carreras`;
+
+CREATE TABLE `carreras` (
+  `id_carrera` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `activo` int(11) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id_carrera`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `carreras` */
+
+insert  into `carreras`(`id_carrera`,`nombre`,`activo`,`created_at`,`updated_at`) values (1,'Tecnologias de la Información y Comunicación',NULL,'2020-12-03 22:24:42','2020-12-03 22:25:03'),(3,'Negocios y Gestion Empresarial',NULL,'2020-12-04 00:07:57','2020-12-04 00:07:57'),(4,'Mecatronica',NULL,'2020-12-04 00:08:10','2020-12-04 00:08:10');
+
+/*Table structure for table `grupos` */
+
+DROP TABLE IF EXISTS `grupos`;
+
+CREATE TABLE `grupos` (
+  `id_grupo` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `activo` int(11) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id_grupo`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `grupos` */
+
+/*Table structure for table `migrations` */
+
+DROP TABLE IF EXISTS `migrations`;
+
+CREATE TABLE `migrations` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `migrations` */
+
+insert  into `migrations`(`id`,`migration`,`batch`) values (1,'2020_12_03_182236_create_usuario_table',1),(2,'2020_12_03_182516_create_alumnos_table',1),(3,'2020_12_03_182622_create_carreras_table',1),(4,'2020_12_03_182642_create_grupos_table',1);
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `users` */
+
+/*Table structure for table `usuario` */
+
+DROP TABLE IF EXISTS `usuario`;
+
+CREATE TABLE `usuario` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `a_paterno` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `a_materno` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `correo` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `contrasena` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `activo` int(11) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `usuario` */
+
+insert  into `usuario`(`id`,`nombre`,`a_paterno`,`a_materno`,`correo`,`contrasena`,`activo`,`created_at`,`updated_at`) values (2,'Jeusu','MOdel','Wasss','rosa@gmail.com','$2y$10$d2INDUcj0K0DNtduHcpkiu1sJNCZ28GnhWBsHjw2RQolDGijzEZMK',1,'2020-12-04 00:38:26','2020-12-04 00:38:26'),(3,'Emmanuel','Damian','Peña','edp12011995@gmail.com','$2y$10$gUVZGHX5ed799xB3jKzVUuplYdi2DKZfioPoKbd6HvKTaZarw2ioK',1,'2020-12-04 00:52:37','2020-12-04 00:52:37');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
